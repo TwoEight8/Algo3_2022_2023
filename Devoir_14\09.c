413756efe9a0e65f6a939b085672a1e2848b05b5
Florent Thomas <113096768+TwoEight8@users.noreply.github.com>
2022-09-14 11:11:51 +0200

    Create Devoir_14\09.c

---
+++ Devoir_14\09.c
@@ -0,0 +1,36 @@
+int somme(int nb1, int nb2)
+{
+    int result1;
+    result1 = (nb1+nb2);
+    return(result1);
+}
+
+int soustraction (int nb5, int nb6)
+{
+    int result3;
+    result3 = (nb5-nb6);
+    return(result3);
+}
+
+int divise(int nb3, int nb4)
+{
+    int result2;
+    result2 = (nb3/nb4);
+    return(result2);
+}
+
+int main ()
+{
+    printf("%d",somme(10,8));
+    printf("\n");
+    printf("%d",somme(15,25));
+    printf("\n");
+    printf("%d",divise(9,3));
+    printf("\n");
+    printf("%d",divise(12.4));
+    printf("\n");
+    printf("%d",soustraction(25,15));
+    printf("\n");
+    printf("%d",soustraction(30,25));
+    return 0;
+}
